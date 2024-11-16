a4a085616581283d50b11ab116de084eb23790be
Yehan Wasura <yehantest@gmail.com>
2024-11-16 21:15:42 +0530

    add item now can ad item to the database

--- gui/tools/add_items.py
+++ gui/tools/add_items.py
@@ -291,24 +291,24 @@ class AddItems:
             widget.destroy()
 
     def add_item(self):
-        print("Attributes:")
-        attrs = {}
-        for attribute, value_entry in self.collect_attributes.items():
-            # Determine if the attribute is created new or selected
-            if attribute.winfo_manager():  # Check if the name_entry is visible
-                attribute_name = attribute.get()  # This is the name_entry
-                attrs[attribute_name] = value_entry.get()
-            else:
-                attribute_name = attribute.get()  # This is the attribute_combo
-                attrs[attribute_name] = value_entry.get()
-                # UI is messed up, its the other way around so that need to fixed.
+        try:
+            attrs = {}
+            for attribute, value_entry in self.collect_attributes.items():
+                # Determine if the attribute is created new or selected
+                if attribute.winfo_manager():  # Check if the name_entry is visible
+                    attribute_name = attribute.get()  # This is the name_entry
+                    attrs[attribute_name] = value_entry.get()
+                else:
+                    attribute_name = attribute.get()  # This is the attribute_combo
+                    attrs[attribute_name] = value_entry.get()
+                    # UI is messed up, its the other way around so that need to fixed.
 
-        print("Common Item:", self.common_checkbox.get())
-        print("Item Name:", self.item_name.get())
-        print("Attributes:", attrs)
-        item = create_item(self.item_name.get(), self.common_checkbox.get(), attrs)
-        if item:
-            messagebox.showinfo("Success", f"Successfully created an item name:{self.item_name.get()}")
-            self.display()
-        else:
-            messgaebox.showerror("Failed", f"Failed to create an item name:{self.item_name.get()}")
+            item = create_item(self.item_name.get(), self.common_checkbox.get(), attrs)
+            if item:
+                messagebox.showinfo("Success", f"Successfully created an item name:{self.item_name.get()}")
+                self.collect_attributes.clear()
+                self.display()
+            else:
+                messgaebox.showerror("Failed", f"Failed to create an item name:{self.item_name.get()}")
+        except:
+            messagebox.showerror("Error", "Failed to create an item")
